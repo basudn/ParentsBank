9b5d1063f8294dd241fa879edcfb76c444a110aa
Manoj Kumar Billa <billamr@mail.uc.edu>
2016-09-06 14:12:47 -0400

    Added all the models.

--- ParentsBank/Models/Account.cs
+++ ParentsBank/Models/Account.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System;
 
 namespace ParentsBank.Models
 {
     public class Account
     {
+
+
+
+
     }
 }
---
+++ ParentsBank/Models/Wishlist_Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParentsBank.Models
+{
+    public class Wishlist_Item
+    {
+    }
+}
