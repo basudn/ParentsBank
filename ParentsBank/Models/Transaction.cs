73bbac9b9acd28bdc09dfbbd46e9e64b2c9e9178
Nikki <Nikki@NIKKI-HP>
2016-09-20 19:49:07 -0400

    Need to upload changes, probably irrelevant now.

--- ParentsBank/Models/Transaction.cs
+++ ParentsBank/Models/Transaction.cs
@@ -12,14 +12,27 @@ namespace ParentsBank.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public virtual int AccountId { get; set; }
+
+
         public virtual AccountDetails Account { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:MM/dd/yyyy}")]
+
+        //A transaction date cannot be in the future and cannot be before the current year
+
+        
         public DateTime TransactionDate { get; set; }
         [CustomValidation(typeof(CustomFieldValidations), "ValidateNotZero")]
+
+        //A transaction cannot be for a 0.00 amount
+        //The amount of the transaction is negative(indicates a debit) or positive(indicates a credit)
+        //A debit cannot be for more than the current account balance
         public double Amount { get; set; }
+
         [Required]
         [StringLength(200)]
+        [DataType(DataType.MultilineText)]
         public string Note { get; set; }
     }
 }
--- ParentsBank/Models/WishlistItem.cs
+++ ParentsBank/Models/WishlistItem.cs
@@ -16,10 +16,15 @@ namespace ParentsBank.Models
         public virtual AccountDetails Account { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime DateAdded { get; set; }
+
+        //Cost must be provided
         public double Cost { get; set; }
+
         [Required]
         [StringLength(200)]
         public string Description { get; set; }
+
+        //Description mus be provided
         [StringLength(100)]
         [CustomValidation(typeof(CustomFieldValidations), "ValidateValidURL")]
         public string Link { get; set; }
